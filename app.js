2dfcae33388649c555dff44ad90b47ee8a3dc386
Alexander Pustosmekhov <a.pustosmekhov@gmail.com>
2022-09-14 14:42:41 +0300

    add file upload

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const cors = require("cors");
 const contactsRouter = require("./src/routes/api/contacts");
 const usersRouter = require("./src/routes/api/users");
 
+const UPLOADS_PATH = "public/avatars";
+
 const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
@@ -13,6 +15,7 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
+app.use("/avatars", express.static(UPLOADS_PATH));
 app.use("/api/contacts", contactsRouter);
 app.use("/api/users", usersRouter);
 
--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -9,7 +9,12 @@ const {
   login,
   logout,
   getCurrentUser,
+  updateAvatar,
 } = require("../../services/usersService");
+const { uploadMiddlewar } = require("../../middlewares/uploadMiddlewar");
+const {
+  compressImgMiddlewar,
+} = require("../../middlewares/compressImgMiddlewar");
 
 const router = express.Router();
 
@@ -17,5 +22,12 @@ router.post("/signup", createUserValidation, signup);
 router.get("/login", loginUserValidation, login);
 router.get("/logout", authMiddlewar, logout);
 router.get("/current", authMiddlewar, getCurrentUser);
+router.patch(
+  "/avatars",
+  authMiddlewar,
+  uploadMiddlewar.single("avatar"),
+  compressImgMiddlewar,
+  updateAvatar
+);
 
 module.exports = router;
